5fb0463b362de2f3edea19688bfe8a0b0ef2d69f
BryanVasquez1006 <bryan.vasqueziraheta@gmail.com>
2025-11-05 19:57:28 -0600

    first commit

---
+++ code-along-header/scripts/navigation.js
@@ -0,0 +1,9 @@
+const navbutton = document.querySelector("#ham-btn");
+const navBar = document.querySelector("#nav-bar");
+//Toggle the show class off and on
+
+navbutton.addEventListener("click", () => {
+    navbutton.classList.toggle('show');
+    navBar.classList.toggle('show');
+    
+})
